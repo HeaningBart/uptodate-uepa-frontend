b1b0dc72366ceb93bfc3332d191666883b4b19b3
Gustavo Souza <gustavo.araujo@aluno.uepa.br>
2023-11-06 17:21:55 -0300

    fix to search page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -15,7 +15,14 @@ export async function generateMetadata({ searchParams: { query } }: { searchPara
 export default async function ArticlesPage({ searchParams: { query } }: { searchParams: { query: string } }) {
     await checkStatus()
 
-    const response = await (await fetch(`https://www.uptodate.com/services/app/contents/search/2/json?search=${query}&sp=0&searchType=PLAIN_TEXT&source=USER_PREF&searchControl=TOP_PULLDOWN&searchOffset=1&autoComplete=false&language=pt&max=10&index=&autoCompleteTerm=`)).json()
+    const response = await (await fetch(`https://www.uptodate.com/services/app/contents/search/2/json?search=${query}&sp=0&searchType=PLAIN_TEXT&source=USER_PREF&searchControl=TOP_PULLDOWN&searchOffset=1&autoComplete=false&language=pt&max=10&index=&autoCompleteTerm=`, {
+        next: {
+            revalidate: 0
+        }
+    })).json()
+
+
+    console.log(response.data.searchResults)
 
     return (<>
         <Header />
