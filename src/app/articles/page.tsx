286dac62490b41c871f2514c0f1a3970af7b7ea9
Gustavo Souza <gustavo.araujo@aluno.uepa.br>
2023-11-07 09:04:16 -0300

    fix to search page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import SearchList from '@/components/Search/List'
 import { checkStatus } from '@/services/server/auth'
+import axios from 'axios'
 import { Metadata } from 'next'
 
 
@@ -15,20 +16,14 @@ export async function generateMetadata({ searchParams: { query } }: { searchPara
 export default async function ArticlesPage({ searchParams: { query } }: { searchParams: { query: string } }) {
     await checkStatus()
 
-    const response = await (await fetch(`https://www.uptodate.com/services/app/contents/search/2/json?search=${query}&sp=0&searchType=PLAIN_TEXT&source=USER_PREF&searchControl=TOP_PULLDOWN&searchOffset=1&autoComplete=false&language=pt&max=10&index=&autoCompleteTerm=`, {
-        next: {
-            revalidate: 0
-        }
-    })).json()
+    const response = await axios.get(`https://www.uptodate.com/services/app/contents/search/2/json?search=${query}&sp=0&searchType=PLAIN_TEXT&source=USER_PREF&searchControl=TOP_PULLDOWN&searchOffset=1&autoComplete=false&language=pt&max=10&index=&autoCompleteTerm=`)
 
-
-    console.log(response.data.searchResults)
 
     return (<>
         <Header />
         <main className="bg-[#f6f6f6]">
             <div className="container font-sans">
-                <SearchList data={response.data.searchResults} />
+                <SearchList data={response.data.data.searchResults} />
             </div>
         </main>
     </>)
