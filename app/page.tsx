5a72dbc95ad213125f63047e1e33526683e46ecc
Gustavo Souzavs <gustavo.araujo@aluno.uepa.br>
2024-03-25 13:44:48 -0300

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,32 @@ import './globals.css'
 import Test from '@/components/Test'
 import Header from '@/components/header'
 import { Input } from '@/components/ui/input'
-import { Alert } from '@/components/ui/alert'
+import { getUserData } from '@/services/server'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { get_time_diff } from '@/lib/utils'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'UpToDate do Pará - Início',
   description: 'Artigos por preços acessíveis e com qualidade garantida.',
 }
 
-export default function Home() {
+export default async function Home() {
+  const user = await getUserData()
+
   return (
-    <div className="min-h-[80vh] container flex flex-col items-center justify-center">
+    <div className="min-h-[80vh] container flex flex-col items-center justify-center gap-3">
+      <Alert className="text-primary">
+        <AlertTitle>Opa!</AlertTitle>
+        <AlertDescription>
+          {`Seu benefício acaba ${get_time_diff(
+            user.user?.authorizedUntil!
+          )}. `}
+          <Link className="inline text-blue-600 font-bold" href="/store">
+            Renove agora mesmo clicando aqui!
+          </Link>
+        </AlertDescription>
+      </Alert>
       <form className="w-full" action="/contents/search">
         <Input
           name="search"
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+moment.locale('pt-br')
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function get_time_diff(time: string) {
+  const current = Date.now()
+  const date = new Date(time)
+  if (date.getTime() < current + 259200000) {
+    return moment(time).fromNow()
+  } else {
+    return moment(time).format('L')
+  }
 }
